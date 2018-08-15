f1c0ce3625abfb015756c024ef1abea8914c5c80
Jai Sharma <jaisharma@Jais-MacBook-Pro.local>
2018-08-15 18:41:23 -0500

    fixed commit

---
+++ application.py
@@ -0,0 +1,10 @@
+import os
+
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
