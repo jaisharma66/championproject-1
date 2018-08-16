73517ac4be6682c79efe0a4aefd0e4630ced5205
Jai Sharma <jaisharma@Jais-MBP.attlocal.net>
2018-08-15 23:26:54 -0500

    added new html pages, preparing for API implementation

---
+++ README.md
@@ -0,0 +1,6 @@
+CHANGELOG:
+
+Wed, Aug 15, 2018:
+    Added index.html, layout.html, application.html, and application.py
+
+    Adding basic layout and structure to files
--- application.py
+++ application.py
@@ -8,3 +8,15 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/application")
+def application():
+    return render_template("application.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
